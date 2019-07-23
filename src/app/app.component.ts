7b439221327256c03f5e28643cb7243884dd5bf6
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-07-23 18:20:07 +0530

    Added temp data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   public title: string = "MEAN STACK by Trainees @VC";
+  public trainees: { name: string; id: number }[] = [
+    { name: "Apoorva Singh", id: 1 },
+    { name: "Himanshu Chaddha", id: 2 },
+    { name: "Pankaj Nelson Tirkey", id: 3 },
+    { name: "Randhir Sah", id: 4 },
+    { name: "Sandeep Sarswat", id: 5 }
+  ];
 }
