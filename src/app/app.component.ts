e404f2fc5e937f3a1641884c86f438ee6e4ec11c
Apoorva <sapoorva039@gmail.com>
2019-07-24 11:37:41 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from "@angular/core";
 export class AppComponent {
   public title: string = "MEAN STACK by Trainees @VC";
   public trainees: { name: string; id: number; attendance: string }[] = [
-    { name: "Apoorva Singh", id: 1, attendance: "" },
+    { name: "Apoorva Singh", id: 1, attendance: "Present" },
     { name: "Himanshu Chaddha", id: 2, attendance: "" },
     { name: "Pankaj Nelson Tirkey", id: 3, attendance: "Present" },
     { name: "Randhir Sah", id: 4, attendance: "" },
