2b773678bc9c60a9ffe0a01496c94ef714495d19
Randhir Sah <dav.randhir@gmail.com>
2019-07-24 16:39:42 +0530

    routes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+ selector: "app-root",
+ templateUrl: "./app.component.html",
+ styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   public title: string = "MEAN STACK by Trainees @VC";
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './dashboard/events/events.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { OrdersComponent } from './dashboard/orders/orders.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    EventsComponent,
+    ProfileComponent,
+    OrdersComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
