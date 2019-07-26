faa09ac55d978c40ef6fe1ab955b16557018ac30
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-07-26 18:14:18 +0530

    Removed redundant routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
-import {AuthGuard} from './auth.guard';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { AdminComponent } from "./admin/admin.component";
+import { AuthGuard } from "./auth.guard";
 
-
-  const routes: Routes = [
-  {path : '' , component:LoginComponent},
-  {path : 'login' , component:LoginComponent},
-  {path :  'admin',component:AdminComponent, canActivate:[AuthGuard]}
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "admin", component: AdminComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-
 })
-export class AppRoutingModule { }
-export const routingComponent =[LoginComponent,AdminComponent]
+export class AppRoutingModule {}
+export const routingComponent = [LoginComponent, AdminComponent];
