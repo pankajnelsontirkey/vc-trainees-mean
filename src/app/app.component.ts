157197db78057ce9eac4be2b00cd87a44669843b
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-07-26 17:35:56 +0530

    Tested changes by himanshu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
- selector: "app-root",
- templateUrl: "./app.component.html",
- styleUrls: ["./app.component.css"]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  public title: string = "MEAN STACK by Trainees @VC";
-  public trainees: { name: string; id: number; attendance: string }[] = [
-    { name: "Apoorva Singh", id: 1, attendance: "Present" },
-    { name: "Himanshu Chaddha", id: 2, attendance: "Present" },
-    { name: "Pankaj Nelson Tirkey", id: 3, attendance: "Present" },
-    { name: "Randhir Sah", id: 4, attendance: "Present" },
-    { name: "Sandeep Sarswat", id: 5, attendance: "Present" }
-  ];
-}
+export class AppComponent {}
